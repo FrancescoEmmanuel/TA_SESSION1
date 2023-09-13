aaa591f2362b20c0546b8b7b4f52ed3d99fca8a9
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2023-09-13 15:45:18 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App{
+    public static void main(String[] args){
+        
+        Car myCar = new Car(200, "Xenia", 2001);  
+
+        myCar.honk();
+        myCar.startEngine();
+        myCar.getName();
+    }
+
+    
+}
